9bc02693846c59187f8a0221dcf9c80cdcf443d6
Ian Clark <ian.m.clark@gmail.com>
2018-10-01 12:54:47 -0700

    Random city and tour generation

--- TravelingSalesman/Program.cs
+++ TravelingSalesman/Program.cs
@@ -6,14 +6,27 @@ using System.Windows.Forms;
 
 namespace TravelingSalesman {
     static class Program {
+        private static int CITIES_COUNT = 15;
+        private static int IMAGE_WIDTH = 512;
+        private static int IMAGE_HEIGHT = 512;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main() {
-            Application.EnableVisualStyles();
+            TSPTour tour = TSPTour.RandomTour(TSPTour.RandomCities(CITIES_COUNT, IMAGE_WIDTH, IMAGE_HEIGHT));
+            foreach(TSPCity city in tour.GetCities()) {
+                Console.WriteLine("({0}, {1})", city.x, city.y);
+            }
+            foreach(int city in tour.GetTour()) {
+                Console.Write("{0} ", city);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+            /*Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1());*/
         }
     }
 }
---
+++ TravelingSalesman/TravelingSalesman.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelingSalesman {
+    struct TSPCity {
+        public int x;
+        public int y;
+    }
+
+    class TSPTour {
+        private TSPCity[] cities;
+        private int[] tour;
+
+        public TSPTour(TSPCity[] Cities, int[] Tour) {
+            this.cities = Cities;
+            this.tour = Tour;
+        }
+
+        public TSPCity[] GetCities() {
+            return this.cities;
+        }
+
+        public int[] GetTour() {
+            return this.tour;
+        }
+
+        public static TSPCity[] RandomCities(int Count, int MaxX, int MaxY) {
+            TSPCity[] cities = new TSPCity[Count];
+            Random rand = new Random();
+            for(int i=0; i<Count; i++) {
+                TSPCity city = new TSPCity();
+                city.x = rand.Next(MaxX);
+                city.y = rand.Next(MaxY);
+                cities[i] = city;
+            }
+            return cities;
+        }
+
+        public static TSPTour RandomTour(TSPCity[] Cities) {
+            int[] tour = new int[Cities.Length];
+            Random rand = new Random();
+            int i;
+            for(i=0; i<tour.Length; i++) {
+                tour[i] = -1;
+            }
+            for(int city=0; city<Cities.Length; city++) {
+                do {
+                    i = rand.Next(tour.Length);
+                } while (tour[i] != -1);
+                tour[i] = city;
+            }
+            return new TSPTour(Cities, tour);
+        }
+    }
+}
